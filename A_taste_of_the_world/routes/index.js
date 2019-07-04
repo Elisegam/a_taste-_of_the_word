6b8a955f25c07561d987edc01d31fd15c2e46c75
Elise <elisegamassa@gmail.com>
2019-07-04 10:41:35 +0200

    3rd update elise

--- A_taste_of_the_world/routes/index.js
+++ A_taste_of_the_world/routes/index.js
@@ -1,26 +1,43 @@
 const express = require("express");
 const router = new express.Router();
+const mongoose = require("mongoose");
 
 const recipeModel = require("../models/Recipe.js");
 
 /* GET home page */
+// router.get("/", (req, res, next) => {
+//   var q = req.query.q;
+//   Recipe.find(
+//     {
+//       $text: { $search: q }
+//     },
+//     {
+//       _id: 0,
+//       __v: 0
+//     },
+//     function(err, data) {
+//       res.json(data);
+//     }
+//   )
+//     .then(recipes => console.log(recipes))
+//     .catch(e => console.log(e));
+// });
+
+//   recipeModel.createIndex({
+//   Name: "text",
+//   Region: "text",
+//   image: "text"
+// });
+
+// const Recipe = mongoose.model("Recipe", recipeModel);
+
+// Recipe.find({
+//   $text: { $search: term }
+// })
+//   .then(recipes => console.log(recipes))
+//   .catch(e => console.log(e));
+
 router.get("/", (req, res, next) => {
-  //   var q = req.query.q;
-  //   Recipe.find(
-  //     {
-  //       $text: { $search: q }
-  //     },
-  //     {
-  //       _id: 0,
-  //       __v: 0
-  //     },
-  //     function(err, data) {
-  //       res.json(data);
-  //     }
-  //   )
-  //     .then(recipes => console.log(recipes))
-  //     .catch(e => console.log(e));
-  // });
   res.render("index");
 });
 
@@ -51,19 +68,5 @@ cookie
   .catch(err => {
     console.log("An error occured", err);
   });
-
-// recipeModel.createIndex({
-//   Name: "text",
-//   Region: "text",
-//   image: "text"
-// });
-
-// const Recipe = mongoose.model("Recipe", recipeModel);
-
-// Recipe.find({
-//   $text: { $search: term }
-// })
-//   .then(recipes => console.log(recipes))
-//   .catch(e => console.log(e));
 
 module.exports = router;
